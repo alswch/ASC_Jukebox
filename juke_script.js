be56c7bd37cd15241e2b111d892e0c3146b6bc25
alswch <alswchung@gmail.com>
2017-06-07 12:53:44 -0400

    added music files(mp3). updated music info saving into empty array

--- juke_script.js
+++ juke_script.js
@@ -1,23 +1,38 @@
 var jukebox = {
+  musicLibrary: [],
+
   initialize: function() {
     this.activateSongModal();
   },
 
   activateSongModal: function() {
-    console.log("==activateSongModal==");
+    // console.log("==activateSongModal==");
     var self = this;    //this is the key code
-    console.log("this:", this);
-    console.log("self:", self);
-    document.getElementById("findBtn").addEventListener("click", function(){
-      console.log("-- click:findBtn --");
-      console.log("this:", this);
-      console.log("self:", self);
-      self.showModal();
+    // console.log("this:", this);
+    // console.log("self:", self);
+    document.getElementById("saveBtn").addEventListener("click", function(){
+      // console.log("-- click:saveBtn --");
+      // console.log("this:", this);
+      // console.log("self:", self);
+      self.savedMusicInfo();
     })
   },
 
-  showModal: function() {
-    console.log("showModal");
+  savedMusicInfo: function() {
+    // console.log("showModal");
+    var title = document.getElementById('title').value;
+    var artist = document.getElementById('artist').value;
+    var url = document.getElementById('url').value;
+    var nextMusic = new jukebox.Music(title, artist, url);
+    console.log(nextMusic);
+    jukebox.musicLibrary.push(nextMusic);
+    console.log(jukebox.musicLibrary);
+  },
+// ====== Music constructor ======
+  Music: function(title, artist, url) {
+    this.title = title;
+    this.artist = artist;
+    this.url = url;
   }
 }
 jukebox.initialize();
